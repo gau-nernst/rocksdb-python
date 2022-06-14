68c76390b1c1d82d8b8a245f6409e3dfc3a1e987
Thien Tran <gau.nernst@yahoo.com.sg>
2022-06-14 17:28:28 +0800

    clean up code

--- setup.py
+++ setup.py
@@ -8,25 +8,22 @@ IS_WIN = platform.system() == "Windows"
 __version__ = "0.0.1"
 
 
-libraries = ["rocksdb"]
-extra_win_libs = [
-    "Rpcrt4",  # for RpcStringFreeA, UuidCreateSequential, UuidToStringA
-    "Shlwapi",  # for PathIsDirectoryA, PathIsRelativeA
-]
-extra_unix_libs = [
-    "z",    # zlib
-    "bz2",  # bzip2
-]
-if IS_WIN:
-    libraries.extend(extra_win_libs)
-else:
-    libraries.extend(extra_unix_libs)
+def get_libraries():
+    libraries = ["rocksdb"]
+    extra_win_libs = ["Rpcrt4", "Shlwapi"]  # for port_win.cc
+    extra_unix_libs = ["z", "bz2"]
+    if IS_WIN:
+        libraries.extend(extra_win_libs)
+    else:
+        libraries.extend(extra_unix_libs)
+    return libraries
+
 
 ext_m = Pybind11Extension(
     "rocksdb_python",
     sorted(glob("src/*.cpp")),
     include_dirs=["rocksdb/include"],
-    libraries=libraries,
+    libraries=get_libraries(),
     library_dirs=["./"],
     define_macros=[("VERSION_INFO", __version__)],
     cxx_std=17,
