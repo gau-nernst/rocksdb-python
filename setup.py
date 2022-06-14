c2ba4d9a567d6659d2f7db7d477ded7f1617f5fb
Thien Tran <gau.nernst@yahoo.com.sg>
2022-06-14 13:45:39 +0800

    exclude zlib and bzip2 from Windows build

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
+import platform
 from glob import glob
 
 from pybind11.setup_helpers import Pybind11Extension
 from setuptools import setup
 
+IS_WIN = platform.system() == "Windows"
 __version__ = "0.0.1"
 
 
+libraries = ["rocksdb"]
+if not IS_WIN:
+    libraries.extend(["z", "bz2"])
 ext_m = Pybind11Extension(
     "rocksdb_python",
     sorted(glob("src/*.cpp")),
     include_dirs=["rocksdb/include"],
-    libraries=["rocksdb", "z", "bz2"],
+    libraries=libraries,
     library_dirs=["./"],
-    define_macros = [('VERSION_INFO', __version__)],
+    define_macros=[("VERSION_INFO", __version__)],
     cxx_std=17,
 )
 ext_modules = [ext_m]
