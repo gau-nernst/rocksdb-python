ee1dbed14446e798899114ccc3c2fb665fe706cd
Thien Tran <gau.nernst@yahoo.com.sg>
2023-03-28 13:57:00 +0800

    prefer homebrew static libs

--- setup.py
+++ setup.py
@@ -18,14 +18,19 @@ IS_LINUX = platform.system() == "Linux"
 IS_MACOS = platform.system() == "Darwin"
 IS_WIN = platform.system() == "Windows"
 
-SNAPPY_LIB = os.environ.get("SNAPPY_LIB", "snappy")
-LZ4_LIB = os.environ.get("LZ4_LIB", "lz4")
-ZLIB_LIB = os.environ.get("ZLIB_LIB", "z")
-ZSTD_LIB = os.environ.get("ZSTD_LIB", "zstd")
-BZ2_LIB = os.environ.get("BZ2_LIB", "bz2")
+_default_lib_names = dict(
+    rocksdb="rocksdb",
+    snappy="snappy",
+    lz4="lz4",
+    zlib="z",
+    zstd="zstd",
+    bz2="bz2",
+)
+lib_names = {k: os.environ.get(k.upper() + "_LIB", v) for k, v in _default_lib_names.items()}
 
 include_dirs = []
 library_dirs = []
+extra_objects = []  # for static linking
 
 
 def add_path(path, include="include", lib="lib"):
@@ -45,6 +50,14 @@ if IS_MACOS:
         proc = subprocess.run(["brew", "--prefix"], check=True, capture_output=True)
         HOMEBREW_PREFIX = Path(proc.stdout.decode().strip())
         add_path(HOMEBREW_PREFIX)
+
+        # prefer static libs from Homebrew
+        for k, v in lib_names.items():
+            static_lib_path = HOMEBREW_PREFIX / "lib" / f"lib{v}.a"
+            if static_lib_path.exists():
+                extra_objects.append(str(static_lib_path))
+                lib_names[k] = None
+
     except FileNotFoundError:  # brew is not installed
         pass
 
@@ -58,7 +71,7 @@ if IS_WIN:
         add_path(VCPKG_PREFIX / "installed" / "x64-windows-static-md")
 
 
-libraries = ["rocksdb", SNAPPY_LIB, LZ4_LIB, ZLIB_LIB, ZSTD_LIB, BZ2_LIB]
+libraries = [lib for lib in lib_names.values() if lib is not None]
 if IS_WIN:
     libraries.extend(["Rpcrt4", "Shlwapi"])  # for port_win.cc
     libraries.append("Cabinet")  # for XPRESS
@@ -72,6 +85,7 @@ ext = Pybind11Extension(
     library_dirs=library_dirs,
     define_macros=[("VERSION_INFO", __version__)],
     cxx_std=17,
+    extra_objects=extra_objects,
 )
 
 setup(
